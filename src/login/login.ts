4588a50c0f38ca7dbd4620874ca21ba86781b9d1
neoeahit <neoeahit@gmail.com>
2016-05-29 16:52:48 -0400

    Finally working with fb integration :) . Suppa happygit add .git add .

--- src/login/login.ts
+++ src/login/login.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, enableProdMode, Injectable, OnInit } from '@angular/core';
 import { Router, RouterLink } from '@angular/router-deprecated';
 import { CORE_DIRECTIVES, FORM_DIRECTIVES } from '@angular/common';
-import { Http, Headers } from '@angular/http';
 import { contentHeaders } from '../common/headers';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+
+
+import {Observable} from 'rxjs/Observable';
+import {Subject } from 'rxjs/Subject';
+import { Http, Headers, Response, RequestOptions, HTTP_PROVIDERS, URLSearchParams } from '@angular/http';
 
 let styles   = require('./login.css');
 let template = require('./login.html');
+
+@Injectable()
+class SubmitUserData {
+  constructor(private http: Http) {}
+
+  submitUserData(data) {
+    const endpoint = 'http://localhost:4000/auth/facebook';
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+    console.log(data);
+    return this.http
+      .post(endpoint, data, options)
+      .map(res => res.json());
+    }
+
+  private handleError (error: any) {
+  // In a real world app, we might use a remote logging infrastructure
+  // We'd also dig deeper into the error to get a better message
+  let errMsg = (error.message) ? error.message :
+    error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+  console.error(errMsg); // log to console instead
+  console.log("i am here")
+  return Promise.reject(errMsg);
+}
+}
 
 declare const FB:any;
 
@@ -13,12 +44,13 @@ declare const FB:any;
   selector: 'login',
   directives: [RouterLink, CORE_DIRECTIVES, FORM_DIRECTIVES ],
   template: template,
-  styles: [ styles ]
+  styles: [ styles ],
+  providers: [HTTP_PROVIDERS, SubmitUserData]
 })
 
-export class Login {
+export class Login implements OnInit{
 
-    constructor() {
+    constructor(private _api: SubmitUserData) {
         FB.init({
             appId      : '1034066206674244',
             cookie     : false,  // enable cookies to allow the server to access
@@ -32,16 +64,26 @@ export class Login {
         FB.login();
     }
 
+
+
     statusChangeCallback(resp) {
         if (resp.status === 'connected') {
             FB.api('/me', {fields: 'email,birthday,picture,first_name,gender,last_name,location'},
             function(response) {
-            localStorage.setItem('jwt_stades', JSON.stringify(response));
+                localStorage.setItem('jwt_stades', JSON.stringify(response));
             });
+            if(localStorage.getItem('jwt_stades')){
+                this._api.submitUserData(localStorage.getItem('jwt_stades')).subscribe(
+              data => { console.log("success")},
+              err => console.error(err),
+              () => console.log('done posting data')
+            );
+              }
         }else if (resp.status === 'not_authorized') {
         }else {
         }
     };
+
     ngOnInit() {
         FB.getLoginStatus(response => {
             this.statusChangeCallback(response);
